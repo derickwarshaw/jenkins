ae30a1a33fb56fa6ed5e73a28bd6f220c65d2193
derickwarshaw <dwarshaw@madmobile.com>
2016-10-15 16:13:51 -0400

    - added some notes

--- App/Services/jobs.js
+++ App/Services/jobs.js
@@ -6,3 +6,21 @@ export default {
     return api.post(`/job/${job}/build`);
   }
 };
+
+/**
+ * # check if job exists
+ curl -XGET 'http://jenkins/checkJobName?value=yourJobFolderName' --user user.name:YourAPIToken
+
+ # with folder plugin
+ curl -s -XPOST 'http://jenkins/job/FolderName/createItem?name=yourJobName' --data-binary @config.xml -H "Content-Type:text/xml" --user user.name:YourAPIToken
+
+ # without folder plugin
+ curl -s -XPOST 'http://jenkins/createItem?name=yourJobName' --data-binary @config.xml -H "Content-Type:text/xml" --user user.name:YourAPIToken
+
+ # create folder
+ curl -XPOST 'http://jenkins/createItem?name=FolderName&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22FolderName%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK' --user user.name:YourAPIToken -H "Content-Type:application/x-www-form-urlencoded"
+
+
+ # see http://jenkins/api/
+
+ */
