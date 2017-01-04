d03b9636a318910e1d4b48618a173426e7122626
derickwarshaw <dwarshaw@madmobile.com>
2017-01-04 14:52:31 -0500

    - Getting rid of the white flash on the loading screen by adjusting the rootView.backgroundColor in AppDelegate.m

--- ios/jenkins/AppDelegate.m
+++ ios/jenkins/AppDelegate.m
@@ -24,7 +24,7 @@
                                                       moduleName:@"jenkins"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.259f green:0.051f blue:0.063f alpha:0.20];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
