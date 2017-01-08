015a01c0242fa621524b71ba3f54ea5a0961bf16
derickwarshaw <dwarshaw@madmobile.com>
2017-01-08 04:01:02 -0500

    - Unused code removal

--- App/Containers/BuildScreen.js
+++ App/Containers/BuildScreen.js
@@ -2,8 +2,6 @@ import React, {PropTypes} from 'react';
 import { ScrollView, Text, Image, View } from 'react-native';
 import { Images } from '../Themes';
 import { connect } from 'react-redux';
-import { Actions as NavigationActions } from 'react-native-router-flux';
-import Button from '../Components/Button';
 
 // Styles
 import styles from './Styles/PresentationScreenStyle';
